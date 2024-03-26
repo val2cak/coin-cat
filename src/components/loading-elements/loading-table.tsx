dccd9de75108fa2ad04c36c7a8412f6334612d76
val2cak <valentina.tucak99@gmail.com>
2024-03-26 19:19:32 +0100

    fix: styling

--- src/components/loading-elements/loading-table.tsx
+++ src/components/loading-elements/loading-table.tsx
@@ -2,11 +2,6 @@ const LoadingTable = () => {
   return (
     <div className='overflow-x-auto border border-light border-opacity-10 rounded-md'>
       <table className='w-full table-auto'>
-        <thead>
-          <tr className='bg-transparent text-light border-b border-b-light border-opacity-10'>
-            <th className='px-8 py-8'></th>
-          </tr>
-        </thead>
         <tbody>
           <tr className='bg-transparent'>
             <td className='dot-spinner'>
